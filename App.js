70a683b0d2e0e35bbcfeea2ae1b398270409cd92
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-26 18:30:31 +0100

    First  animation

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { StyleSheet, Animated, View } from 'react-native';
 
-export default function App() {
-  return <View style={style.square} />;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, { toValue: { x: 200, y: 300 } }).start();
+  }
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={style.square} />
+      </Animated.View>
+    );
+  }
 }
 
 const style = StyleSheet.create({
